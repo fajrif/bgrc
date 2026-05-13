4dd06ecd1e0a10ff451cdc29a04e1c8e5eb69b66
Fajri Fachriansyah <fajri82@gmail.com>
2026-05-13 23:50:35 +0700

    Move secrets to ENV vars and remove unused Google OAuth

--- config/configatron/development.rb
+++ config/configatron/development.rb
@@ -5,6 +5,6 @@
 configatron.site_url = "localhost:3000"
 configatron.midtrans_api_url = "https://app.sandbox.midtrans.com/snap/v1/transactions"
 configatron.midtrans_js_file = "https://app.sandbox.midtrans.com/snap/snap.js"
-configatron.midtrans_merchant_id = "***REMOVED***"
-configatron.midtrans_client_key = "***REMOVED***"
-configatron.midtrans_server_key = "***REMOVED***"
+configatron.midtrans_merchant_id = ENV['MIDTRANS_MERCHANT_ID']
+configatron.midtrans_client_key = ENV['MIDTRANS_CLIENT_KEY']
+configatron.midtrans_server_key = ENV['MIDTRANS_SERVER_KEY']
